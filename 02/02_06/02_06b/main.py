95b92b944b9f4a6d49b1d45b39a940a5ab2f9515
Aram S. Openden <aram.openden@gmail.com>
2024-05-20 16:04:13 +0000

    LiL: PF_DS - section 2.5 - tuples

--- 02/02_06/02_06b/main.py
+++ 02/02_06/02_06b/main.py
@@ -1,1 +1,15 @@
 # Tuples are immutable array-like structures
+
+point = (5, 2)
+
+x = point[0]
+y = point[1]
+
+def calc_square_properties(side_len):
+    area = side_len * side_len
+    perimiter = side_len * 4
+    return (area, perimiter)  
+
+calcRes = calc_square_properties(5)
+print("Area: ", calcRes[0])
+print("Perimteter: ", calcRes[1])
