e61e16992b21b1862efb4d31dc5019318901ebcb
Aram S. Openden <aram.openden@gmail.com>
2024-06-24 17:06:41 +0000

    section_2.7: search lists

--- 02/02_07/02_07b/main.py
+++ 02/02_07/02_07b/main.py
@@ -1,1 +1,19 @@
 # Linear Search
+
+# Aram workfile
+
+my_list = [8, 0, 3, 6, 15, 7]
+
+ITEM = 7
+
+def search(item, listy):
+    for elem in listy:
+        if (item == ITEM):
+            return True
+    return False
+
+print(search(ITEM, my_list))    
+
+## use built-in python search function
+ITEM_IDX = my_list.index(ITEM)
+print(ITEM_IDX)
